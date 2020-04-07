8ef56a2adc9fa931f8356ff299357051b69338af
Sone <sthaya@gmail.com>
2020-04-07 16:47:45 -0700

    created load, resize event listener

--- js/index.js
+++ js/index.js
@@ -8,7 +8,66 @@ function changeLogo(event) {
 }
 topLogo.addEventListener('mouseover', changeLogo);
 
+// wheel event listener to change image back to original
 function changeBack(event) {
   topLogo.src = "img/fun-bus.jpg"
 }
-topLogo.addEventListener('mouseout', changeBack)
+topLogo.addEventListener('wheel', changeBack);
+
+// drag and drop for P tags bottom section
+const draggables = document.querySelectorAll('.draggable');
+const destination = document.querySelectorAll('.destination');
+
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging')
+  })
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging')
+  })
+})
+
+destination.forEach(container => {
+  container.addEventListener('dragover', (e) => {
+    e.preventDefault()
+    const draggable = document.querySelector('.dragging')
+    container.appendChild(draggable)
+  })
+})
+
+// focus 
+// const logoHeading = document.getElementById('logo')
+
+// function logoHead() {
+//   logoHeading.style.backgroundColor = 'blue';
+// }
+// logoHeading.addEventListener('focus', logoHead)
+
+
+// resize
+window.addEventListener("resize", () => {
+  document.getElementById('text').innerHTML = Math.random()
+})
+
+// load
+window.addEventListener('load', (event) => {
+  alert('page is fully loaded');
+});
+
+
+// change bottom image from double click event
+const bottomImage = document.getElementById('bottom-image');
+
+function changeBottomImage() {
+  bottomImage.src = "img/beach.jpg"
+}
+bottomImage.addEventListener('dblclick', changeBottomImage);
+
+// keydown event to reload page when ESC is pressed
+function escKey(event) {
+  if (event.key === 'Escape') {
+    location.reload()
+  }
+}
+document.addEventListener('keydown', escKey)
