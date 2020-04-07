d1b8d97a8ef0915b30c2c5f8fd676cff5077f308
Sone <sthaya@gmail.com>
2020-04-07 13:19:25 -0700

    added more pictures, mouse over to change top picture

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+// mouse over to change top logo image
+const topLogo = document.getElementById('top-logo');
+
+function changeLogo(event) {
+  topLogo.src = "img/bus2.jpg";
+}
+topLogo.addEventListener('mouseover', changeLogo);
+
+function changeBack(event) {
+  topLogo.src = "img/fun-bus.jpg"
+}
+topLogo.addEventListener('mouseout', changeBack)
